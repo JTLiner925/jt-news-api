d77e4a3d4811f067220a099ea27b2a00a913f44a
JTLiner925 <[jtliner925@gmail.com]>
2020-03-04 14:23:38 -0600

    adding functions

---
+++ index.js
@@ -0,0 +1,38 @@
+const apiKey = 'bf4ad93afca94d0a8c4df0261f6de6cb';
+const searchURL = 'https://newsapi.org/v2/everything';
+
+function formatQueryParams(params){
+  const queryItems = Object.keys(params)
+    .map(key => `${key}=${params[key]}`);
+  return queryItems.join('&');
+}
+
+function getNews(query, maxResults=10) {
+  const params = {
+    q: query,
+    language: 'en',
+  };
+  const queryString = formatQueryParams(params);
+  const url = searchURL + '?' + queryString;
+  console.log(url);
+
+  const options = {
+    headers: new Headers({
+      "X-Api-Key": apiKey})
+  };
+
+  fetch(url, options)
+    .then(response => response.json())
+    .then(responseJson => console.log(responseJson));
+}
+
+function watchForm(){
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    const maxResults = $('#js-max-results').val();
+    getNews(searchTerm, maxResults);
+  });
+}
+
+$(getNews);
